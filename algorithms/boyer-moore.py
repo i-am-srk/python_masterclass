d961c27dc43433e11fded94ae82bc3172dca1cc7
i-am-srk <shah.rukh.khan.IITian@gmail.com>
2025-01-13 09:11:18 +0530

    bayer-moore algo in Q(n) time and O(1) space complexity

---
+++ algorithms/boyer-moore.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        candidate, count = None, 0
+
+        # find the majority candidate
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += 1 if candidate == num else -1
+
+        # validate occurences
+        if nums.count(candidate) > len(nums) // 2:
+            return candidate
+        return None
+
+        
+obj = Solution()
+nums = [3,2,3]
+print(f"ans : {obj.majorityElement(nums)}")  
