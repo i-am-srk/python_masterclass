5eb79176a5043f5b203709f14b91c4c4fa13abc3
i-am-srk <shah.rukh.khan.IITian@gmail.com>
2025-01-18 16:28:56 +0530

    solved

--- data structures/arrays/remove_duplicates.py
+++ data structures/arrays/remove_duplicates.py
@@ -10,11 +10,11 @@ class Solution(object):
 
         k=1
         for i in range(1, len(nums)):
-            if nums[i] != nums[i-1]:
-                nums[k] = nums[i]
+            if nums[i] != nums[i-1] and nums[i] > nums[k-1]:
+                nums[k], nums[i] = nums[i], nums[k]
                 k+=1
         return k
     
 obj = Solution()
-nums = [1,1,1,2,2]
-print(f"ans : {obj.removeDuplicates(nums)} | nums : {nums}")
+nums = [0,0,1,1,1,2,2,3,3,4]
+print(f"ans : {obj.removeDuplicates(nums)}")
